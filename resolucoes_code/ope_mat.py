f61ac334435b9e5ae8f10fddb5c2f175472541da
Junior Sarmento <145942234+sarmentojr@users.noreply.github.com>
2024-06-26 19:22:08 +0000

    adicionando operações matemáticas

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,22 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite um número inteiro: "))
+
+operacao = input("Digite a operação desejada (+, -, *, /): ")
+
+
+if operacao == "+":
+  resultado = numero1 + numero2
+elif operacao == "-":
+  resultado = numero1 - numero2
+elif operacao == "*":
+  resultado = numero1 * numero2
+elif operacao == "/":
+  resultado = numero1 / numero2
+else:
+  print("Operação inválida!")
+  exit()
+
+
+print("O resultado da operação é:", resultado)
