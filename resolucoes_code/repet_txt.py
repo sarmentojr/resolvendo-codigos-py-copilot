d5cb76ba30adb61398fa72818b07487fee2ca632
Junior Sarmento <145942234+sarmentojr@users.noreply.github.com>
2024-06-26 18:10:30 +0000

    alterando o texto do segundo desafio

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,1 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+#  Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado.
