cbccb10182de078afdfb4d5091794435b98ed7f2
Junior Sarmento <145942234+sarmentojr@users.noreply.github.com>
2024-06-26 14:49:40 -0300

    inserindo código para concatenar

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+info_nome = input("Qual o seu nome?")
+info_sobrenome = input("Qual o seu sobrenome?")
+
+concatenando_nome_e_sobrenome = info_nome + " " + info_sobrenome
+
+print("O seu nome completo é ", concatenando_nome_e_sobrenome)
