59b3b7a4e32c2771d45b51a58aff37b02432db41
Junior Sarmento <145942234+sarmentojr@users.noreply.github.com>
2024-06-26 18:16:04 +0000

    código para repetir string

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,9 @@
 #  Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado.
+
+string = input("Digite uma string: ")
+numero_inteiro = int(input("Digite um número inteiro: "))
+numero_repeticoes = 4
+
+string_repetida = string * numero_repeticoes
+
+print("String repetida:", string_repetida)
